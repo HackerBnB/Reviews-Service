b18e5a95c1b58286e00920d1b45e8f90b1945f69
priya suresh <priyasuresh@MacBook-Pro-2.local>
2018-08-22 20:30:38 -0700

    connect aws database

--- models/index.js
+++ models/index.js
@@ -1,3 +1,23 @@
+const { Pool } = require('pg');
+
+const config = {
+  host: 'ec2-54-193-12-133.us-west-1.compute.amazonaws.com',
+  user: 'postgres',
+  database: 'homelyre',
+  port: 5432,
+};
+
+const pool = new Pool(config);
+
+pool.connect();
+
+console.log('connected database');
+module.exports = {
+  pool: pool,
+};
+
+/*
+connect sql
 const Pool = require('pg').Pool;
 
 const config = {
@@ -16,9 +36,6 @@ console.log('connected database');
 module.exports = {
   pool: pool,
 };
-
-/*
-connect sql
 */
 
 // 
